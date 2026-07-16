46350f5c4676d3016ac91230aa9f53f83123eabe
dashakaznacheeva92-ai <dasha.kaznacheeva92@gmail.com>
2026-07-16 11:17:44 +0100

    Update app/main.py
    Co-authored-by: Vasyl Smutok <vasyl.smutok@gmail.com>

--- app/main.py
+++ app/main.py
@@ -29,9 +29,7 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int,
                  coords: list | None = None) -> None:
-        if coords is None:
-            coords = [0, 0, 0]
-        super().__init__(name=name, weight=weight, coords=coords)
+        super().__init__(name=name, weight=weight, coords=coords or [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
